98640cb012e8aefb5052ff3a9686d84f58c131c5
Dobrica Savic <dobrica.savic@sysdig.com>
2023-02-03 14:20:28 +0100

    try adding query and list pools

--- neo4j/query.go
+++ neo4j/query.go
@@ -4,28 +4,84 @@ import (
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
 
+type keyValue map[string]interface{}
+type records [][]interface{}
+
 // RETURN 1;
 func (n *Neo4j) Return() (*int64, error) {
 	driver := *n.driver
-    session := driver.NewSession(neo4j.SessionConfig{})
-    defer session.Close()
-    result, err := session.ReadTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+	session := driver.NewSession(neo4j.SessionConfig{})
+	defer session.Close()
+	result, err := session.ReadTransaction(func(tx neo4j.Transaction) (interface{}, error) {
 		var result, err = tx.Run("RETURN 1;", nil)
 		// driver error
 		if err != nil {
 			return nil, err
 		}
 
-    	if record, err := result.Single(); err != nil {
+		if record, err := result.Single(); err != nil {
 			return nil, err
 		} else {
 			return record.Values[0].(int64), err
 		}
 	})
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 	count := result.(int64)
-    return &count, nil
+	return &count, nil
+}
+
+func (n *Neo4j) Read(cypherQuery string, params keyValue) (records, error) {
+	driver := *n.driver
+	session := driver.NewSession(neo4j.SessionConfig{})
+	defer session.Close()
+
+	result, err := session.ReadTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+		var result, err = tx.Run(cypherQuery, params)
+		// driver error
+		if err != nil {
+			return nil, err
+		}
+
+		records := make(records, 0)
+		for result.Next() {
+			record := result.Record()
+			records = append(records, record.Values)
+		}
+		return records, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return result.(records), nil
+}
+
+func (n *Neo4j) ListPools() (records, error) {
+	driver := *n.driver
+	session := driver.NewSession(neo4j.SessionConfig{})
+	defer session.Close()
+
+	result, err := session.ReadTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+
+		var result, err = tx.Run("CALL dbms.listPools();", nil)
+		// driver error
+		if err != nil {
+			return nil, err
+		}
+
+		records := make(records, 0)
+		for result.Next() {
+			record := result.Record()
+			records = append(records, record.Values)
+		}
+		return records, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return result.(records), nil
 }
