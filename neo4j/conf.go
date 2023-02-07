f5f7cfd9ff44dfa8d21870822bd9a23aed663d3b
Dobrica Savic <dobrica.savic@sysdig.com>
2023-02-07 12:53:58 +0100

    run without config file to support distributed runs

--- neo4j/conf.go
+++ neo4j/conf.go
@@ -2,17 +2,18 @@ package neo4j
 
 import (
 	"fmt"
+
 	"github.com/spf13/viper"
 )
 
 const (
-	NEO4J_ADDR = "NEO4J_ADDR"
-	NEO4J_USERNAME = "NEO4J_USERNAME"
-	NEO4J_PASSWORD = "NEO4J_PASSWORD"
-	NEO4J_DATABASE = "NEO4J_DATABASE"
-	NEO4J_DEFAULT_ADDR = "neo4j://localhost:7687"
+	NEO4J_ADDR             = "NEO4J_ADDR"
+	NEO4J_USERNAME         = "NEO4J_USERNAME"
+	NEO4J_PASSWORD         = "NEO4J_PASSWORD"
+	NEO4J_DATABASE         = "NEO4J_DATABASE"
+	NEO4J_DEFAULT_ADDR     = "neo4j://localhost:7687"
 	NEO4J_DEFAULT_USERNAME = "neo4j"
-	// note this is here for consistancy but 
+	// note this is here for consistancy but
 	// by default neo4j will reject all cmds using the default password
 	NEO4J_DEFAULT_PASSWORD = "neo4j"
 	NEO4J_DEFAULT_DATABASE = "neo4j"
@@ -31,8 +32,8 @@ func NewConf() *Conf {
 	conf.SetConfigType("env")
 	conf.AutomaticEnv()
 	err := conf.ReadInConfig() // Find and read the config file
-	if err != nil { // Handle errors reading the config file
-		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	if err != nil {            // Handle errors reading the config file
+		fmt.Errorf("Fatal error config file: %w \n", err)
 	}
 	return &Conf{
 		conf,
